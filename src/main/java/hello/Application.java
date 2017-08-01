1cdbee53b1f2145dd3786707324e38067994ae25
raghuraman1 <raghuraman1@yahoo.com>
2017-08-02 00:29:04 +0530

    minor change

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,10 @@
 package hello;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
@@ -11,8 +16,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
+	public String home() throws SocketException {
+		StringBuilder sb= new StringBuilder();
+		Enumeration e = NetworkInterface.getNetworkInterfaces();
+		while(e.hasMoreElements())
+		{
+		    NetworkInterface n = (NetworkInterface) e.nextElement();
+		    Enumeration ee = n.getInetAddresses();
+		    while (ee.hasMoreElements())
+		    {
+		        InetAddress i = (InetAddress) ee.nextElement();
+		        sb.append(i.getHostAddress());
+		        sb.append("\n");
+		        
+		    }
+		}
+		
+		return sb.toString();
+		
 	}
 
 
